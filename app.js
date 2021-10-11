db7877a616a70a28f34fa06fc20a55776fdaee13
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-11 08:40:50 -0600

    single level JSON data

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let start = '{"favColor":"orange","favSeason":"Fall"}';
+// let start = '{"favColor":"orange","favSeason":"Fall"}';
 
-let myObject = JSON.parse(start);
+// let myObject = JSON.parse(start);
+
+// console.log(myObject);
+
+// let myString = JSON.stringify(myObject);
+
+// myObject = JSON.parse(myString);
+
+// console.log(myObject);
+
+let theData1 = `{"first":"Paul","last":"Cheney","city":"Salem"}`;
+
+let myObject = JSON.parse(theData1);
 
 console.log(myObject);
 
-let myString = JSON.stringify(myObject);
-
-myObject = JSON.parse(myString);
-
-console.log(myObject);
+document.querySelector('p').innerHTML = myObject.first;
